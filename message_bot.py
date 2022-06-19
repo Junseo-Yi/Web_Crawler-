778c4c1e730b1b753b187257fce4f8c3322afb11
Junseo_Yi <mfgim1726@gmail.com>
2022-06-19 11:20:30 +0900

    slacke bot added

---
+++ message_bot.py
@@ -0,0 +1,46 @@
+from slack_sdk import WebClient
+
+token = "xoxb-3674127147559-3688741549554-vRFVaB00ynnrecO3C4NYyZAT"
+slack = SlackAPI(token)
+
+class SlackAPI:
+    """
+    슬랙 API 핸들러
+    """
+    def __init__(self, token):
+        # 슬랙 클라이언트 인스턴스 생성
+        self.client = WebClient(token)
+
+    def get_channel_id(self, channel_name):
+        """
+        슬랙 채널ID 조회
+        """
+        # conversations_list() 메서드 호출
+        result = self.client.conversations_list()
+        # 채널 정보 딕셔너리 리스트
+        channels = result.data['channels']
+        # 채널 명이 'test'인 채널 딕셔너리 쿼리
+        channel = list(filter(lambda c: c["name"] == channel_name, channels))[0]
+        # 채널ID 파싱
+        channel_id = channel["id"]
+        return channel_id
+
+    def post_thread_message(self, channel_id, text):
+        """
+        슬랙 채널 내 메세지의 Thread에 댓글 달기
+        """
+        # chat_postMessage() 메서드 호출
+        result = self.client.chat_postMessage(
+            channel=channel_id,
+            text = text
+        )
+        return result
+
+channel_name = "크롤링"
+text = "자동 생성 문구 테스트"
+
+# 채널ID 파싱
+channel_id = slack.get_channel_id(channel_name)
+
+# 댓글 달기
+slack.post_thread_message(channel_id, text)
